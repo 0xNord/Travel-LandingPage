de52abe1000458c4eaee652cd87292fd939fdfa7
Ivan <58911140+0xNord@users.noreply.github.com>
2023-06-01 21:44:36 +0300

    fix header slider
    cause there is duplicated names in activitiesSlider.js

--- js/activitiesSlider.js
+++ js/activitiesSlider.js
@@ -1,24 +1,24 @@
 let a_slideIndex = 1;
 
-function showSlides(n) {
+function a_showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("activities-slider-slide");
-  if (n > slides.length) {a_slideIndex = 1}
-  if (n < 1) {a_slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  let a_slides = document.getElementsByClassName("activities-slider-slide");
+  if (n > a_slides.length) {a_slideIndex = 1}
+  if (n < 1) {a_slideIndex = a_slides.length}
+  for (i = 0; i < a_slides.length; i++) {
     // slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  a_slides[slideIndex-1].style.display = "block";
 }
 showSlides(a_slideIndex);
 
 // Next/previous controls
 function aplusSlides(n) {
-  showSlides(a_slideIndex += n);
+  a_showSlides(a_slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(a_slideIndex = n);
+  a_showSlides(a_slideIndex = n);
 }
 
